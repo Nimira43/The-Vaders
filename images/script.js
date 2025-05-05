cfc7969b98da2ca9070bcdf0613cf09fd6da3009
Nimira43 <n.rabone72@aol.com>
2025-05-05 20:17:52 +0100

    Canvas initialisation

--- images/script.js
+++ images/script.js
@@ -56,7 +56,7 @@ class Wave {
 }
 
 class Game {
-  constructor() {
+  constructor(canvas) {
   
   }
   render() {
@@ -72,3 +72,22 @@ class Game {
 
   }
 }
+
+window.addEventListener('load', function () {
+  const canvas = this.document.getElementById('canvas1')
+  const ctx = canvas.getContext('2d')
+  canvas.width = 600
+  canvas.height = 800
+  ctx.fillStyle = 'white'
+  ctx.strokeStyle = 'white'
+  ctx.lineWidth = 5
+
+  const game = new Game(canvas)
+  
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    game.render(ctx)
+    requestAnimationFrame(animate)
+  }
+  animate()
+})

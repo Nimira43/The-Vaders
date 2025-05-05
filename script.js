5cff08f1ff1978038068813dab5865f0158fb9ba
Nimira43 <n.rabone72@aol.com>
2025-05-06 00:58:29 +0100

    Setting up deltaTime

--- script.js
+++ script.js
@@ -303,11 +303,14 @@ window.addEventListener('load', function() {
 
   const game = new Game(canvas)
   
-  function animate() {
+  let lastTime = 0
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime
+    lastTime = timeStamp
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    game.render(ctx)
+    game.render(ctx, deltaTime)
     requestAnimationFrame(animate)
   }
 
-  animate()
+  animate(0)
 })

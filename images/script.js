fbe5e3daaebc10aadb875654cd5789056e254502
Nimira43 <n.rabone72@aol.com>
2025-05-05 20:24:36 +0100

    Setting up player constructor

--- images/script.js
+++ images/script.js
@@ -1,6 +1,10 @@
 class Player {
   constructor(game) {
-
+    this.game = game
+    this.width = 100
+    this.height = 100
+    this.x = this.game.width * 0.5 - this.width * 0.5
+    this.y = this.game.height - this.height;
   }
   draw() {
 

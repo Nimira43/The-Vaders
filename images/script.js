4caf9508733e76d7ea371d5eaecc16b8f4cf0968
Nimira43 <n.rabone72@aol.com>
2025-05-05 20:01:19 +0100

    Setting up Player and Projectile classes - placeholder at moment.

---
+++ images/script.js
@@ -0,0 +1,32 @@
+class Player {
+  constructor() {
+
+  }
+  draw() {
+
+  }
+  update() {
+
+  }
+  shoot() {
+    
+  }
+}
+
+class Projectile {
+  constructor() {
+
+  }
+  draw() {
+
+  }
+  update() {
+
+  }
+  start() {
+
+  }
+  reset() {
+
+  }
+}

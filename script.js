b32a6250fcbd7af8438e72dbbc1a12a4e2fe234c
Nimira43 <n.rabone72@aol.com>
2025-05-06 01:09:06 +0100

    Updated Player class draw method

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 class Player {
   constructor(game) {
     this.game = game
-    this.width = 100
-    this.height = 100
+    this.width = 140
+    this.height = 120
     this.x = this.game.width * 0.5 - this.width * 0.5
     this.y = this.game.height - this.height;
-    this.speed = 10
+    this.speed = 5
     this.lives = 3
     this.maxLives = 10
     this.image = document.getElementById('player')
-    this.jet_image = document.getElementById('player_jets')
+    this.jets_image = document.getElementById('player_jets')
     this.frameX = 0
     this.jetsFrame = 1  
   }
 
   draw(context) {
-    context.fillRect(this.x, this.y, this.width, this.height)
+    if (this.game.keys.indexOf('1') > -1) {
+      this.frameX = 1
+    } else {
+      this.frameX = 0
+    }
+
+    context.drawImage(this.jets_image, this.jetsFrame * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+    context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
   }
 
   update() {

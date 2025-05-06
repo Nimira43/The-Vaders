01628b21b921518222c303be968a4e5e1512c480
Nimira43 <n.rabone72@aol.com>
2025-05-06 01:40:04 +0100

    Updated the create metho in the Wave class

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ class Wave {
       for (let x = 0; x < this.game.columns; x++) {
         let enemyX = x * this.game.enemySize
         let enemyY = y * this.game.enemySize
-        this.enemies.push(new Enemy(this.game, enemyX, enemyY))
+        this.enemies.push(new Beetlemorph(this.game, enemyX, enemyY))
       }
     }
   }
